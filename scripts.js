17104a7397a78f4de2ecf1d59f3d790b595ab56d
shannontreadway <shannon.treadway@gmail.com>
2020-10-04 20:33:16 -0500

    Up button functionality added

--- scripts.js
+++ scripts.js
@@ -1,5 +1,10 @@
 // Write your JavaScript code here.
 // Remember to pay attention to page loading!
+let offsetLeft = 0;
+let offsetUp = 0;
+let shuttleButtonHeight = 0;
+
+
 function init() {
     let takeoff = document.getElementById("takeoff");
     let landing = document.getElementById("landing");
@@ -11,13 +16,15 @@ function init() {
     let flightStatus = document.getElementById("flightStatus");
     let shuttleBackground = document.getElementById("shuttleBackground");
     let spaceShuttleHeight = document.getElementById("spaceShuttleHeight");
+    let rocket = document.getElementById("rocket");
     
     takeoff.addEventListener("click", function(event){
         let confirmation = window.confirm("Confirm that the shuttle is ready for take off.")
         if (confirmation) {
             flightStatus.innerHTML = "Shuttle in flight.";
             shuttleBackground.style.backgroundColor = "blue";
-            spaceShuttleHeight.innerHTML = "10,000";
+            spaceShuttleHeight.innerHTML = 10000;
+            shuttleButtonHeight = 10000;
         };
     });
 
@@ -25,7 +32,7 @@ function init() {
         window.alert("The shuttle is landing. Landing gear engaged.");
         flightStatus.innerHTML = "The shuttle has landed.";
         shuttleBackground.style.backgroundColor = "green";
-        spaceShuttleHeight.innerHTML = "0";
+        spaceShuttleHeight.innerHTML = 0;
     });
 
     missionAbort.addEventListener("click", function(event){
@@ -33,10 +40,29 @@ function init() {
         if (confirmation) {
             flightStatus.innerHTML = "Mission aborted.";
             shuttleBackground.style.backgroundColor = "green";
-            spaceShuttleHeight.innerHTML = "0";
+            spaceShuttleHeight.innerHTML = 0;
         };
     });
-    
+
+    up.addEventListener("click", function(event){
+        shuttleButtonHeight += 10000;
+        spaceShuttleHeight.innerHTML = shuttleButtonHeight;
+        offsetUp -= 10;
+        rocket.style.position = 'relative';
+        rocket.style.top = (offsetUp)+'px';
+    });
+
+    down.addEventListener("click", function(event){
+
+    });
+
+    left.addEventListener("click", function(event){
+
+    });
+
+    right.addEventListener("click", function(event){
+
+    });
 }
 
 
