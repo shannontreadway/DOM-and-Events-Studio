258c1c43bd947f338cdbd4b9c4a337a0e0a8809a
shannontreadway <shannon.treadway@gmail.com>
2020-10-04 19:21:11 -0500

    TakeOff Button functionality added

--- scripts.js
+++ scripts.js
@@ -1,5 +1,33 @@
 // Write your JavaScript code here.
 // Remember to pay attention to page loading!
-window.addEventListener("load", function(){
-  console.log('window loaded');  
-});
+function init() {
+    let takeoff = document.getElementById("takeoff");
+    let landing = document.getElementById("landing");
+    let missionAbort = document.getElementById("missionAbort");
+    let up = document.getElementById("up");
+    let down = document.getElementById("down");
+    let left = document.getElementById("left");
+    let right = document.getElementById("right");
+    let flightStatus = document.getElementById("flightStatus");
+    let shuttleBackground = document.getElementById("shuttleBackground");
+    let spaceShuttleHeight = document.getElementById("spaceShuttleHeight");
+    
+    takeoff.addEventListener("click", function(event){
+        let confirmation = window.confirm("Confirm that the shuttle is ready for take off.")
+        if (confirmation) {
+            flightStatus.innerHTML = "Shuttle in flight.";
+            shuttleBackground.style.backgroundColor = "blue";
+            spaceShuttleHeight.innerHTML = "10,000";
+        }
+    })
+}
+
+
+
+
+
+
+
+
+
+window.onload = init;
